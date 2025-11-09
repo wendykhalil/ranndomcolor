960d32663fe50638c0e32f07ee2fe803541cc172
khalilwendy <khalilwendy15@gmail.com>
2025-11-09 19:06:41 +0100

    lastv

--- script.js
+++ script.js
@@ -20,28 +20,51 @@ generate_btn.addEventListener("click", () => {
     })
 })
 
-function copy(element) {
-    // on récupère le texte hex de CETTE box
-    const hex_value = element.querySelector(".hex-value").textContent;
 
-    // on copie dans le presse-papiers
-    navigator.clipboard.writeText(hex_value)
-        .then(() => {
-            // confirmation : tu peux changer ceci (alert, console, ou un petit tooltip)
-            alert(`Couleur ${hex_value} copiée !`);
-        })
-        .catch(() => {
-            alert("Erreur lors de la copie");
-        });
-}
 
-// On lie la fonction copy à chaque bouton via forEach (même méthode que generate)
-box_color.forEach((element) => {
-    const copyBtn = element.querySelector(".copy-btn");
-    if (copyBtn) {
-        copyBtn.addEventListener("click", () => {
-            copy(element); // on appelle copy en lui passant la box courante
-        });
+
+
+function transfer(icon) {
+    
+    if (icon.classList.contains("copy-btn")) {
+        icon.classList.remove("far", "fa-copy");
+        icon.classList.add("fas", "fa-check");
+        icon.style.color = "green";
+
+        
+        setTimeout(() => {
+            icon.classList.remove("fas", "fa-check");
+            icon.classList.add("far", "fa-copy");
+            icon.style.color = "";
+        }, 1000);
     }
+}
+function copy(element) {
+    
+    const hex_color = element.querySelector(".hex-value").textContent;
+    const icon = element.querySelector(".copy-btn");
+
+    if (hex_color) {
+        navigator.clipboard.writeText(hex_color)
+            .then(() => {
+               
+                transfer(icon);
+            })
+            .catch(() => {
+                alert("⚠️ error");
+            });
+    } else {
+        alert("⚠️ no color to copy");
+    }
+}
+   
+    
+
+box_color.forEach(element => {
+    const color_box = element.querySelector(".color");
+    const icon = element.querySelector(".copy-btn");
+
+    color_box.addEventListener("click", () => copy(element));
+    icon.addEventListener("click", () => copy(element));
 });
 
